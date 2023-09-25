9313682da710e497c8c36d9156fccc97f3bbb38e
plkmnb100 <pcy06@kakao.com>
2023-09-25 22:03:50 +0900

    fix: gunicorn worker gRPC session issue

--- micro/auth/client.py
+++ micro/auth/client.py
@@ -2,6 +2,17 @@ import grpc
 from .declaration.auth_pb2_grpc import AuthServiceStub
 from env import MicroEnv
 
+STORED_CLIENT = None
 
-channel = grpc.aio.insecure_channel(f"{MicroEnv.AUTH_HOST}:{MicroEnv.AUTH_PORT}")
-client = AuthServiceStub(channel)
+
+async def generate_client():
+    global STORED_CLIENT
+
+    if STORED_CLIENT:
+        return STORED_CLIENT
+
+    channel = grpc.aio.insecure_channel(f"{MicroEnv.AUTH_HOST}:{MicroEnv.AUTH_PORT}")
+    client = AuthServiceStub(channel)
+
+    STORED_CLIENT = client
+    return client
--- micro/auth/services/getuser.py
+++ micro/auth/services/getuser.py
@@ -1,7 +1,8 @@
-from ..client import client
+from ..client import generate_client
 from ..declaration.auth_pb2 import GetUserRequest
 
 
 async def get_user(userid: int) -> dict:
+    client = await generate_client()
     response = await client.GetUser(GetUserRequest(userid=userid))
     return response.user if response.success else None
