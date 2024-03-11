f9017d2dc98f2062663a916f07c5ec73ff615192
plkmnb100 <pcy06@kakao.com>
2024-03-11 09:24:01 +0900

    fix: wrong month format in fetch  meal

--- routes/meal.py
+++ routes/meal.py
@@ -16,7 +16,7 @@ async def get_meal_month(month: int, userid: int = Depends(RequireAuth)):
     if not user:
         raise HTTPException(status_code=404, detail="USER_NOT_FOUND")
 
-    meal = await Meal.get(datetime.now().year, month)
+    meal = await Meal.get(datetime.now().year, str(month).zfill(2))
 
     return {
         "message": "SUCCESS",
